b7967522b86cb8b69d218e4bf060ea7ec7465bb9
vagashkov <vitali.agashkov@gmail.com>
2022-10-23 21:05:15 +0300

    Update user registration form

--- auth_test/forms.py
+++ auth_test/forms.py
@@ -2,9 +2,9 @@ from django import forms
 
 
 class RegistrationForm(forms.Form):
-    email = forms.EmailField(widget=forms.EmailInput(attrs={'placeholder': 'Email'}))
-    username = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Username'}))
-    password1 = forms.CharField(widget=forms.PasswordInput())
-    password2 = forms.CharField(widget=forms.PasswordInput())
-    is_agreed = forms.BooleanField(widget=forms.CheckboxInput())
+    email = forms.EmailField(widget=forms.EmailInput(attrs={'placeholder': 'Email'}), required=True)
+    username = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Username'}), required=True)
+    password1 = forms.CharField(widget=forms.PasswordInput(), required=True)
+    password2 = forms.CharField(widget=forms.PasswordInput(), required=True)
+    is_agreed = forms.BooleanField(widget=forms.CheckboxInput(), required=True)
 
